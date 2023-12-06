2d5ff037e0c283fd6edb405bf9b9c7a752ee9bd7
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-07 00:59:08 +0300

    adding handler methods for sendOTP and VarifyOTP

--- api/handler.go
+++ api/handler.go
@@ -1,1 +1,46 @@
-package api;
+package api
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+	"github.com/Danitilahun/OTPVerification-GO/model"
+	"github.com/gin-gonic/gin"
+)
+
+const appTimeout = time.Second * 10
+
+func (app *Config) sendSMS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		_, cancel = context.WithTimeout(context.Background(), appTimeout)
+
+		var payload model.OTPData;
+		defer cancel();
+		app.validateBody(c, &payload);
+		// send sms
+		
+		_, err := app.twilioSendOTP(payload.PhoneNumber);
+		if err != nil {
+			app.errorJSON(c, err);
+			return;
+		}
+		app.successJSON(c, "OTP send Successful", http.StatusAccepted);
+	}
+};
+func (app *Config) verifySMS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		_, cancel := context.WithTimeout(context.Background(), appTimeout)
+
+		var payload model.VerifyOTPData;
+		defer cancel();
+		app.validateBody(c, &payload);
+		// verify sms
+		_, err := app.twilioVerifyOTP(payload.User.PhoneNumber, payload.Code);
+		if err != nil {
+			app.errorJSON(c, err);
+			return;
+		}
+		app.successJSON(c, "OTP verified Successful", http.StatusAccepted);
+	}
+};
