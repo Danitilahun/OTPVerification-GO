f1c29b986a415291d1049d714928647e5a3127ed
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-06 22:54:06 +0300

    completing .env file access function using os and godotenv module

--- api/config.go
+++ api/config.go
@@ -1,1 +1,36 @@
 package api
+
+import (
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+func envACCOUNTID() string {
+	err:= godotenv.Load();
+	if err != nil{
+		log.Fatalln("Error loading .env file")
+		log.Fatal("Error loading .env file")
+	}
+	return os.Getenv("TWILIO_ACCOUNT_SID")
+}
+
+
+func envAUTHTOKEN() string {
+	err:= godotenv.Load();
+	if err != nil{
+		log.Fatalln("Error loading .env file")
+		log.Fatal("Error loading .env file")
+	}
+	return os.Getenv("TWILIO_AUTHTOKEN")
+}
+
+
+func envSERVICEID() string {
+	err:= godotenv.Load();
+	if err != nil{
+		log.Fatalln("Error loading .env file")
+		log.Fatal("Error loading .env file")
+	}
+	return os.Getenv("TWILIO_SERVICES_ID")
+}
