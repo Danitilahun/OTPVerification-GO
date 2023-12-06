c4b134ed1bb4e0ece551f4e8c1c50ada46c95684
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-06 22:56:59 +0300

    remove redundancy in config function while loading env env file for checking error

--- api/config.go
+++ api/config.go
@@ -6,31 +6,26 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func envACCOUNTID() string {
-	err:= godotenv.Load();
-	if err != nil{
-		log.Fatalln("Error loading .env file")
-		log.Fatal("Error loading .env file")
+func loadEnvVariables() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error loading .env file:", err)
 	}
+}
+
+func envACCOUNTID() string {
+	loadEnvVariables()
 	return os.Getenv("TWILIO_ACCOUNT_SID")
 }
 
 
 func envAUTHTOKEN() string {
-	err:= godotenv.Load();
-	if err != nil{
-		log.Fatalln("Error loading .env file")
-		log.Fatal("Error loading .env file")
-	}
+	loadEnvVariables()
 	return os.Getenv("TWILIO_AUTHTOKEN")
 }
 
 
 func envSERVICEID() string {
-	err:= godotenv.Load();
-	if err != nil{
-		log.Fatalln("Error loading .env file")
-		log.Fatal("Error loading .env file")
-	}
+	loadEnvVariables()
 	return os.Getenv("TWILIO_SERVICES_ID")
 }
