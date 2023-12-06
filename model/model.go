d372631cec01f8a1af64a63d8950aa4943974b3d
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-07 00:23:01 +0300

    adding otp models

--- model/model.go
+++ model/model.go
@@ -1,1 +1,9 @@
 package model
+
+type OTPData struct {
+	phoneNumber string  `json: "phoneNumber, omitempty" validate:"required`;
+}
+type VerifyOTPData struct {
+	User *OTPData  `json: "user, omitempty" validate:"required`;
+	Code string  `json: "code, omitempty" validate:"required`;
+}
