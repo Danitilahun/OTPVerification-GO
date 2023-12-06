0a2e5242ac038386fbea20135b326b093ae3b23f
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-07 00:41:01 +0300

    adding validate body function to validate the request body and assign it to my model and also check the validity of the field using validator package

--- api/helper.go
+++ api/helper.go
@@ -1,1 +1,23 @@
 package api;
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+var validate = validator.New();
+func (app *Config) validateBody(c *gin.Context , data any) error{
+	err := c.BindJSON(&data);
+	if err != nil {
+		return err
+	}
+
+	err = validate.Struct(data);
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
