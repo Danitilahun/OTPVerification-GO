949b40a25961a6739959e4ad96f6aab3f4cf7ed0
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-07 00:49:49 +0300

    adding errorJson and successJson

--- api/helper.go
+++ api/helper.go
@@ -2,7 +2,6 @@ package api;
 
 import (
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
@@ -27,3 +26,14 @@ func (app *Config) validateBody(c *gin.Context , data any) error{
 	return nil
 }
 
+
+func (app *Config) errorJSON(c *gin.Context,err error ,  status ...int) {
+	statusCode := http.StatusBadRequest;
+	if len(status) > 0 {
+		statusCode = status[0];
+	}
+	c.JSON(statusCode, jsonResponse{Status: statusCode, Message: err.Error()});
+}
+
+func (app *Config) successJSON(c *gin.Context, data any, status int) {
+	c.JSON(status, jsonResponse{Status: status, Data: data});}
