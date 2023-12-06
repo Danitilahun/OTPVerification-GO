efeb6ea9f7bc2e3a8bc89473944ff222b8603fb4
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-07 01:33:55 +0300

    twilioVerifyOTP and twilioSendOTP method

--- api/service.go
+++ api/service.go
@@ -1,1 +1,44 @@
 package api
+
+import (
+	"errors"
+	"github.com/twilio/twilio-go"
+	twilioApi "github.com/twilio/twilio-go/rest/verify/v2"
+)
+
+var client *twilio.RestClient = twilio.NewRestClientWithParams(twilio.ClientParams{
+	Username: envACCOUNTSID(),
+	Password: envAUTHTOKEN(),
+})
+
+
+
+func (app *Config) twilioSendOTP(phoneNumber string) (string, error) {
+	params := &twilioApi.CreateVerificationParams{}
+	params.SetTo(phoneNumber)
+	params.SetChannel("sms")
+
+	resp, err := client.VerifyV2.CreateVerification(envSERVICESID(), params)
+	if err != nil {
+		return "", err
+	}
+
+	return *resp.Sid, nil
+}
+
+func (app *Config) twilioVerifyOTP(phoneNumber string, code string) error {
+	params := &twilioApi.CreateVerificationCheckParams{}
+	params.SetTo(phoneNumber)
+	params.SetCode(code)
+
+	resp, err := client.VerifyV2.CreateVerificationCheck(envSERVICESID(), params)
+	if err != nil {
+		return err
+	}
+
+	if *resp.Status != "approved" {
+		return errors.New("not a valid code")
+	}
+
+	return nil
+}
